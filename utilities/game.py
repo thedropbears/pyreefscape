c0889947b6468cc192f0ff757400bf02a4a69df2
Beana1234 <asher.broadhead@thedropbears.org.au>
2024-10-22 20:45:25 +1100

    clean up utils

--- utilities/game.py
+++ utilities/game.py
@@ -11,6 +11,7 @@ from wpimath.geometry import (
     Translation3d,
 )
 
+# TODO leaving as placeholder to keep utils
 apriltag_layout = robotpy_apriltag.loadAprilTagLayoutField(
     robotpy_apriltag.AprilTagField.k2024Crescendo
 )
@@ -21,27 +22,12 @@ get_fiducial_pose = typing.cast(
     typing.Callable[[TagId], Pose3d],
     apriltag_layout.getTagPose,
 )
-
+# TODO leaving as placeholder to keep utils
 RED_SPEAKER_POSE = get_fiducial_pose(4)
 BLUE_SPEAKER_POSE = get_fiducial_pose(7)
 
 FIELD_WIDTH = 8.0161
 FIELD_LENGTH = RED_SPEAKER_POSE.x + BLUE_SPEAKER_POSE.x
-
-# Minimum height of the overhanging speaker hood as obtained from a field Onshape model.
-SPEAKER_HOOD_HEIGHT = 2.104883
-SPEAKER_HOOD_WIDTH = 1.05
-SPEAKER_HOOD_DEPTH = 0.456499
-
-BLUE_SPEAKER_TARGET_POSITION = BLUE_SPEAKER_POSE.translation() + Translation3d(
-    SPEAKER_HOOD_DEPTH, 0, 0
-)
-
-RED_SPEAKER_TARGET_POSITION = RED_SPEAKER_POSE.translation() - Translation3d(
-    SPEAKER_HOOD_DEPTH, 0, 0
-)
-
-NOTE_DIAMETER = 0.0254 * 14
 
 
 def field_flip_pose2d(p: Pose2d):
@@ -70,14 +56,3 @@ def field_flip_translation2d(t: Translation2d):
 # This will default to the blue alliance if a proper link to the driver station has not yet been established
 def is_red() -> bool:
     return wpilib.DriverStation.getAlliance() == wpilib.DriverStation.Alliance.kRed
-
-
-def get_goal_speaker_position() -> Translation3d:
-    if is_red():
-        return RED_SPEAKER_POSE.translation()
-
-    return BLUE_SPEAKER_POSE.translation()
-
-
-def translation_to_goal(position: Translation2d) -> Translation2d:
-    return get_goal_speaker_position().toTranslation2d() - position
