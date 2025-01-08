cd5d87e832de0e8f74c6227e64ead12937abde31
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2025-01-08 23:48:01 +1100

    rename swerve motor and cancoder ids

--- ids.py
+++ ids.py
@@ -5,27 +5,27 @@ import enum
 class TalonId(enum.IntEnum):
     """CAN ID for CTRE Talon motor controllers (e.g. Talon FX, Talon SRX)."""
 
-    DRIVE_1 = 1
-    STEER_1 = 5
+    DRIVE_FL = 1
+    STEER_FL = 5
 
-    DRIVE_2 = 2
-    STEER_2 = 6
+    DRIVE_RL = 2
+    STEER_RL = 6
 
-    DRIVE_3 = 3
-    STEER_3 = 7
+    DRIVE_RR = 3
+    STEER_RR = 7
 
-    DRIVE_4 = 4
-    STEER_4 = 8
+    DRIVE_FR = 4
+    STEER_FR = 8
 
 
 @enum.unique
 class CancoderId(enum.IntEnum):
     """CAN ID for CTRE CANcoder."""
 
-    SWERVE_1 = 1
-    SWERVE_2 = 2
-    SWERVE_3 = 3
-    SWERVE_4 = 4
+    SWERVE_FL = 1
+    SWERVE_RL = 2
+    SWERVE_RR = 3
+    SWERVE_FR = 4
 
 
 @enum.unique
