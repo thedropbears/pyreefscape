5bcebc5b04d19b5a5b20019bdcc05b53b88e91de
Hik-ari <hekaaree@gmail.com>
2025-01-09 15:52:07 +1100

    fixed talonfx import

--- components/manipulator.py
+++ components/manipulator.py
@@ -1,14 +1,14 @@
 from magicbot import tunable
 from phoenix6.configs import MotorOutputConfigs
 from phoenix6.controls import Follower, NeutralOut, VoltageOut
-from phoenix6.hardware import TalonFx
+from phoenix6.hardware import TalonFX
 from phoenix6.signals import NeutralModeValue
 
 
 class ManipulatorComponent:
     def __init__(self) -> None:
-        self.left_flywheel = TalonFx(9)
-        self.right_flywheel = TalonFx(10)
+        self.left_flywheel = TalonFX(9)
+        self.right_flywheel = TalonFX(10)
         self.right_flywheel.set_control(Follower(9, True))
         left_flywheel_config = self.left_flywheel.configurator
         right_flywheel_config = self.right_flywheel.configurator
