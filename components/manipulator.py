07f25d1ddf08b8ff261f859da52eb1231d75577e
Hik-ari <hekaaree@gmail.com>
2025-01-09 15:27:08 +1100

    made the manipulator component with motor control

---
+++ components/manipulator.py
@@ -0,0 +1,30 @@
+from magicbot import tunable
+from phoenix6.configs import MotorOutputConfigs
+from phoenix6.controls import Follower, NeutralOut, VoltageOut
+from phoenix6.hardware import TalonFx
+from phoenix6.signals import NeutralModeValue
+
+
+class ManipulatorComponent:
+    def __init__(self) -> None:
+        self.left_flywheel = TalonFx(9)
+        self.right_flywheel = TalonFx(10)
+        self.right_flywheel.set_control(Follower(9, True))
+        left_flywheel_config = self.left_flywheel.configurator
+        right_flywheel_config = self.right_flywheel.configurator
+        motor_config = MotorOutputConfigs()
+        motor_config.neutral_mode = NeutralModeValue.COAST
+        left_flywheel_config.apply(motor_config)
+        right_flywheel_config.apply(motor_config)
+
+        self.desired_flywheel_speed = tunable(0.0)
+        self.left_flywheel.set_control(NeutralOut())
+
+    def spin_flywheels(self) -> None:
+        self.left_flywheel.set_control(VoltageOut(self.desired_flywheel_speed))
+
+    def coast_flywheels(self) -> None:
+        self.left_flywheel.set_control(NeutralOut())
+
+    def execute(self) -> None:
+        pass
