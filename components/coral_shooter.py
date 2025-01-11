6c5c701027847c298c8decbabab21e8678c2b025
Lucien Morey <lucienp@thedropbears.org.au>
2025-01-11 12:10:42 +1100

    make deposit voltage tunable

--- components/coral_shooter.py
+++ components/coral_shooter.py
@@ -1,4 +1,4 @@
-from magicbot import will_reset_to
+from magicbot import tunable, will_reset_to
 from rev import SparkMax
 
 from ids import SparkId
@@ -7,13 +7,14 @@ from ids import SparkId
 class CoralShooterComponent:
 
     voltage_set_point = will_reset_to(0.0)
+    deposit_voltage = tunable(7.0)
 
     def __init__(self):
         self.motor = SparkMax(SparkId.CORAL_SHOOTER, SparkMax.MotorType.kBrushless)
         self.motor.setInverted(False)  # Change if needed-----------------------------
 
     def deploy(self):
-        self.voltage_set_point = 7.0
+        self.voltage_set_point = self.deposit_voltage
 
     def execute(self):
         # set motor
