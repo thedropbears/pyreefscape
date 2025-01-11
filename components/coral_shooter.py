5eb5b98e750ee47153a48e4dcd4d5c6a1e00566a
Lucien Morey <lucienp@thedropbears.org.au>
2025-01-11 12:05:18 +1100

    fix brushless motor decleration

--- components/coral_shooter.py
+++ components/coral_shooter.py
@@ -1,11 +1,11 @@
-from rev import MotorType, SparkMax
+from rev import SparkMax
 
 from ids import SparkId
 
 
 class CoralShooterComponent:
     def __init__(self):
-        self.motor = SparkMax(SparkId.CORAL_SHOOTER, MotorType.kBrushless)
+        self.motor = SparkMax(SparkId.CORAL_SHOOTER, SparkMax.MotorType.kBrushless)
         self.motor.setInverted(False)  # Change if needed-----------------------------
         self.voltage_set_point = 0.0
 
