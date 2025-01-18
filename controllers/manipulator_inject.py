fbad31b094d38fee4dea8782fa79912a88ef8ab8
Lucien Morey <lucien@thedropbears.org.au>
2025-01-18 12:37:56 +1100

    improve scaffolding reflection of shooter sm intent

--- controllers/manipulator_inject.py
+++ controllers/manipulator_inject.py
@@ -9,21 +9,19 @@ class ManipulatorInject(StateMachine):
     chassis_component: ChassisComponent
 
     def __init__(self) -> None:
-        self.set_range = 0.0
-        self.pose = 0.0
+        pass
 
     def shoot(self) -> None:
         self.engage()
 
     @state(first=True)
     def spinning_up(self) -> None:
-        if self.manipulator_component.flywheels_at_speed():
-            self.next_state("shooting")
+        # self.manipulator.set_range(self.calculate_range_to_barge())
+        # if self.manipulator_component.flywheels_at_speed():
+        self.next_state("shooting")
 
     @timed_state(duration=1, must_finish=True)
     def shooting(self) -> None:
         self.manipulator_component.inject()
 
-        self.pose = self.chassis_component.get_pose()
-
-        self.set_range = 1.0
+        # self.manipulator.set_range(self.calculate_range_to_barge())
