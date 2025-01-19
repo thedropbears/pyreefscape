2fc11811a6761182c808ac4f336d79236c6b28e2
DannyPomeranian <danny.ponomarev@thedropbears.org.au>
2025-01-19 20:16:12 +1100

    add an encoder_config position conversion factor

--- components/tilter_component.py
+++ components/tilter_component.py
@@ -1,11 +1,12 @@
-from magicbot import feedback
-from rev import ClosedLoopSlot, SparkMax, SparkMaxConfig
+from magicbot import feedback, tunable
+from rev import ClosedLoopSlot, EncoderConfig, SparkMax, SparkMaxConfig
 from wpilib import DigitalInput
 
 from ids import SparkId
 
 
 class TilterComponent:
+    angle_change_rate_while_zeroing = tunable(0.2)
     tilter_gear_ratio = 20 * 66 / 26
     desired_angle = 0
 
@@ -56,15 +57,23 @@ class TilterComponent:
         self.encoder_1 = self.tilter_1.getEncoder()
         self.encoder_2 = self.tilter_2.getEncoder()
 
+        self.encoder_config = EncoderConfig()
+        self.encoder_config.positionConversionFactor(
+            self.tilter_gear_ratio
+        )  # TODO check if this line is correct
+
+        # self.encoder_1.configure(self.encoder_config) #TODO find out why this line gives an error
+        # self.encoder_2.configure(self.encoder_config) #TODO find out why this line gives an error
+
         self.encoder_1.setPosition(0)
         self.encoder_2.setPosition(0)
 
     def zero_tilter(self) -> None:
         if not self.tilter_at_top_limit():
-            self.desired_angle += 1
+            self.desired_angle += self.angle_change_rate_while_zeroing
         else:
-            self.encoder_1.setPosition(73 * self.tilter_gear_ratio)
-            self.encoder_2.setPosition(73 * self.tilter_gear_ratio)
+            self.encoder_1.setPosition(73)  # TODO check if this line is correct
+            self.encoder_2.setPosition(73)  # TODO check if this line is correct
             self.desired_angle = 73
 
     @feedback
@@ -84,13 +93,15 @@ class TilterComponent:
 
     def execute(self) -> None:
         self.tilter_controller_1.setReference(
-            self.desired_angle * self.tilter_gear_ratio,
+            self.desired_angle
+            * self.tilter_gear_ratio,  # TODO check if this line is correct
             SparkMax.ControlType.kPosition,
             ClosedLoopSlot.kSlot0,
         )
 
         self.tilter_controller_2.setReference(
-            self.desired_angle * self.tilter_gear_ratio,
+            self.desired_angle
+            * self.tilter_gear_ratio,  # TODO check if this line is correct
             SparkMax.ControlType.kPosition,
             ClosedLoopSlot.kSlot0,
         )
