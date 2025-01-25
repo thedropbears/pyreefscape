938de08fdd3b0b074463389e4bd049bda0bbee90
Calvin O. <calvin.oakley@thedropbears.org.au>
2025-01-25 11:35:46 +1100

    changes FLYWHEEL_RPS_TOLERENCE to 1.0

--- components/algae_manipulator.py
+++ components/algae_manipulator.py
@@ -20,7 +20,7 @@ class AlgaeManipulatorComponent:
     injector_inject_speed = tunable(6.0)
     injector_intake_speed = tunable(-0.5)
 
-    FLYWHEEL_RPS_TOLERENCE = 0.5
+    FLYWHEEL_RPS_TOLERENCE = 1.0
     FLYWHEEL_RAMP_TIME = 1
     FLYWHEEL_GEAR_RATIO = 1 / (1.0 / 1.0)
 
@@ -102,6 +102,7 @@ class AlgaeManipulatorComponent:
 
     def inject(self) -> None:
         self.desired_injector_speed = self.injector_inject_speed
+
 
     def intake(self) -> None:
         self.desired_flywheel_speed = self.flywheel_intake_speed
