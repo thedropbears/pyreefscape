ebd7d7b4547d96c46b4076c8578c1cee8e993f7c
DannyPomeranian <danny.ponomarev@thedropbears.org.au>
2025-01-31 19:43:04 +1100

    create feeler component

---
+++ components/feeler.py
@@ -0,0 +1,31 @@
+from magicbot import feedback, tunable
+from wpilib import DigitalInput, Servo
+
+from ids import DioChannel, PwmChannel
+
+
+class FeelerComponent:
+    DETECT_SPEED = tunable(0.6)  # degrees per cycle
+    START_ANGLE = tunable(90)
+    START_OFFEST = tunable(17)
+    START_OFFSET = tunable(17)
+
+    def __init__(self):
+        self.is_inverted = False
+        self.current_angle = 0.0
+
+        self.limit_switch = DigitalInput(DioChannel.FEELER_LIMIT_SWITCH)
+        self.servo = Servo(PwmChannel.FEELER_SERVO)
+
+    @feedback
+    def touching_algae(self) -> bool:
+        return self.limit_switch.get()
+
+    def set_angle(self, rot: float = 0.0):
+        if not self.is_inverted:
+            self.desired_feeler_angle = rot
+        else:
+            self.desired_feeler_angle = 180 - rot
+
+    def execute(self):
+        self.servo.setAngle(self.desired_feeler_angle)
