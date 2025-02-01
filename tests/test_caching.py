e5f8c5af59deae888310992791531cebe1bb522c
David Vo <auscompgeek@users.noreply.github.com>
2025-02-01 11:45:42 +1100

    Create per loop caching method decorator

---
+++ tests/test_caching.py
@@ -0,0 +1,34 @@
+from utilities import caching
+
+
+def test_cache_per_loop():
+    class Test:
+        def __init__(self):
+            self._per_loop_cache = {}
+            self._bare_called = 0
+            self._property_called = 0
+
+        @caching.cache_per_loop
+        def get_bare(self):
+            self._bare_called += 1
+            return self._bare_called
+
+        @property
+        @caching.cache_per_loop
+        def a_property(self):
+            self._property_called += 1
+            return self._property_called
+
+    test = Test()
+    assert test.get_bare() == 1
+    assert test.get_bare() == 1
+    assert test.a_property == 1
+    assert test.a_property == 1
+
+    assert len(test._per_loop_cache) == 2
+    test._per_loop_cache.clear()
+
+    assert test.get_bare() == 2
+    assert test.get_bare() == 2
+    assert test.a_property == 2
+    assert test.a_property == 2
---
+++ utilities/caching.py
@@ -0,0 +1,28 @@
+import functools
+from collections.abc import Callable
+from typing import Any, Protocol, TypeVar
+
+
+class HasPerLoopCache(Protocol):
+    _per_loop_cache: dict[Callable, Any]
+
+
+S = TypeVar("S", bound=HasPerLoopCache)
+T = TypeVar("T")
+
+
+def cache_per_loop(method: Callable[[S], T]) -> Callable[[S], T]:
+    """
+    Cache a getter method on a component until the cache is cleared.
+
+    Stores a cache in a dict called ``_per_loop_cache`` on the instance.
+    This is expected to be cleared in ``robotPeriodic()``.
+    """
+
+    @functools.wraps(method)
+    def wrapper(self: S) -> T:
+        if method not in self._per_loop_cache:
+            self._per_loop_cache[method] = method(self)
+        return self._per_loop_cache[method]
+
+    return wrapper
