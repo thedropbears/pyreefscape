825f246eceb05fd471194d1abf45205f873b45f1
DannyPomeranian <danny.ponomarev@thedropbears.org.au>
2025-02-01 13:32:01 +1100

    fixed typos in feeler state machine

--- controllers/feeler.py
+++ controllers/feeler.py
@@ -19,27 +19,27 @@ class Feeler(StateMachine):
     @state(first=True, must_finish=True)
     def searching(self, initial_call: bool):
         if initial_call:
-            self.current_feeler_algae = self.START_ANGLE + self.START_OFFSET
+            self.feeler_component.current_angle = self.START_ANGLE + self.START_OFFSET
             self.feeler_component.algae_size = 0.0
 
-        if self.current_feeler_angle >= 160:
-            self.current_feeler_angle = 0.0
+        if self.feeler_component.current_angle >= 160:
+            self.feeler_component.current_angle = 0.0
             self.feeler_component.algae_size = 0.0
 
-        self.feeler_component.set_angle(self.current_feeler_angle)
+        self.feeler_component.set_angle(self.feeler_component.current_angle)
 
         if self.feeler_component.touching_algae():
             self.next_state("found")
             return
 
-        self.current_feeler_angle += self.DETECT_SPEED
+        self.feeler_component.current_angle += self.DETECT_SPEED
 
     @state(must_finish=True)
     def found(self):
-        self.feeler_component.algae_size = self.current_feeler_angle
+        self.feeler_component.algae_size = self.feeler_component.current_angle
         self.done()
 
     def done(self) -> None:
         super().done()
         self.feeler_component.set_angle(self.START_ANGLE)
-        self.current_feeler_angle = self.START_ANGLE
+        self.feeler_component.current_angle = self.START_ANGLE
