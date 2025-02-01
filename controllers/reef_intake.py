a6937bf9dbad371c0f685c05ce86e60fe2327411
DannyPomeranian <danny.ponomarev@thedropbears.org.au>
2025-02-01 12:30:48 +1100

    incorporated the feeler state machine into the floor intake

--- controllers/reef_intake.py
+++ controllers/reef_intake.py
@@ -5,6 +5,7 @@ from magicbot import StateMachine, feedback, state, tunable
 from components.algae_manipulator import AlgaeManipulatorComponent
 from components.chassis import ChassisComponent
 from components.wrist import WristComponent
+from controllers.feeler import Feeler
 from utilities import game
 
 
@@ -12,6 +13,7 @@ class ReefIntake(StateMachine):
     algae_manipulator_component: AlgaeManipulatorComponent
     wrist: WristComponent
     chassis: ChassisComponent
+    feeler: Feeler
 
     L2_INTAKE_ANGLE = tunable(math.radians(-40.0))
     L3_INTAKE_ANGLE = tunable(math.radians(-10.0))
@@ -23,6 +25,10 @@ class ReefIntake(StateMachine):
 
     def intake(self) -> None:
         self.engage()
+
+    @feedback
+    def is_L3(self) -> bool:
+        return game.is_L3(game.nearest_reef_tag(self.chassis.get_pose()))
 
     @state(first=True, must_finish=True)
     def intaking(self, initial_call: bool):
@@ -50,11 +56,14 @@ class ReefIntake(StateMachine):
         distance = self.origin_robot_pose.translation() - robot_pose.translation()
 
         if distance.norm() >= self.RETREAT_DISTANCE:
-            self.done()
+            self.next_state("feeling")
 
-    @feedback
-    def is_L3(self) -> bool:
-        return game.is_L3(game.nearest_reef_tag(self.chassis.get_pose()))
+    @state(must_finish=True)
+    def feeling(self, initial_call):
+        if initial_call:
+            self.feeler.feel()
+        elif not self.feeler.is_executing:
+            self.done()
 
     def done(self) -> None:
         super().done()
