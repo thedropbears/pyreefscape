dbab6cbf8dbe9288f464dda56f2adafa64ac0926
Lucien Morey <lucien@thedropbears.org.au>
2025-02-01 15:01:43 +1100

    stop double flipping initial autonomous pose

--- autonomous/base.py
+++ autonomous/base.py
@@ -4,7 +4,6 @@ from choreo.trajectory import SwerveTrajectory
 from magicbot import AutonomousStateMachine, state
 from wpilib import RobotBase, Timer
 from wpimath.controller import PIDController
-from wpimath.geometry import Pose2d
 from wpimath.kinematics import ChassisSpeeds
 
 from components.algae_manipulator import AlgaeManipulatorComponent
@@ -33,11 +32,10 @@ class AutoBase(AutonomousStateMachine):
 
         try:
             self.trajectory = choreo.load_swerve_trajectory(starting_trajectory)
-            self.starting_pose = self.trajectory.get_initial_pose(game.is_red())
+
         except ValueError:
             # If the trajectory is not found, ChoreoLib already prints to DriverStation
             self.trajectory = SwerveTrajectory("", [], [], [])
-            self.starting_pose = None
 
     def setup(self) -> None:
         #  setup path tracking controllers
@@ -49,18 +47,12 @@ class AutoBase(AutonomousStateMachine):
         # configure defaults for pose in sim
 
         # Setup starting position in the simulator
-        starting_pose = self.get_starting_pose()
-        if RobotBase.isSimulation() and starting_pose is not None:
-            self.chassis.set_pose(starting_pose)
-        super().on_enable()
 
-    def get_starting_pose(self) -> Pose2d | None:
-        starting_pose = self.starting_pose
-        if starting_pose is None:
-            return None
-        if game.is_red():
-            starting_pose = game.field_flip_pose2d(starting_pose)
-        return starting_pose
+        self.starting_pose = self.trajectory.get_initial_pose(game.is_red())
+
+        if RobotBase.isSimulation() and self.starting_pose is not None:
+            self.chassis.set_pose(self.starting_pose)
+        super().on_enable()
 
     @state(first=True)
     def initialising(self) -> None:
