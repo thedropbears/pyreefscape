37ec54587dad15627f051fa99f1a24f0c44af559
LH <liam.howe2@education.nsw.gov.au>
2025-02-09 15:11:30 +1100

    Made is_deployed method for climber use motor forward limit switch (which is not set yet)

--- components/climber.py
+++ components/climber.py
@@ -1,4 +1,4 @@
-from magicbot import tunable
+from magicbot import feedback, tunable
 from rev import SparkMax, SparkMaxConfig
 
 from ids import SparkId
@@ -10,8 +10,6 @@ class ClimberComponent:
 
     def __init__(self) -> None:
         self.motor = SparkMax(SparkId.CLIMBER, SparkMax.MotorType.kBrushless)
-        self.deployed = False  # TODO have some way to detect if this happened
-        self.retracted = False  # TODO have some way to detect if this happened
 
         motor_config = SparkMaxConfig()
         motor_config.inverted(True)
@@ -30,25 +28,14 @@ class ClimberComponent:
     def retract(self) -> None:
         self.target_speed = -self.winch_voltage
 
+    @feedback
     def is_deployed(self) -> bool:
-        return self.deployed
-
-    def is_retracted(self) -> bool:
-        return self.retracted
+        return self.motor.getForwardLimitSwitch().get()
 
     def elevation(self) -> float:
         return 0.0
         # current place holder
 
     def execute(self) -> None:
-        if (
-            self.is_deployed()
-            and self.target_speed > 0
-            or self.is_retracted()
-            and self.target_speed < 0
-        ):
-            self.target_speed = 0.0
-            # stop motor if we're already there
-
         self.motor.setVoltage(self.target_speed)
         self.target_speed = 0.0
