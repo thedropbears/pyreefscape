65173a48d9b411ae5aacd63b398a2e7dbcf6e22a
Lucien Morey <lucien@thedropbears.org.au>
2025-02-14 19:55:33 +1100

    remove assumed feed forward

--- components/intake.py
+++ components/intake.py
@@ -44,7 +44,7 @@ class IntakeComponent:
 
         # CG is at 220mm, 2.7kg
         # https://www.reca.lc/arm?armMass=%7B%22s%22%3A2.7%2C%22u%22%3A%22kg%22%7D&comLength=%7B%22s%22%3A0.22%2C%22u%22%3A%22m%22%7D&currentLimit=%7B%22s%22%3A40%2C%22u%22%3A%22A%22%7D&efficiency=90&endAngle=%7B%22s%22%3A90%2C%22u%22%3A%22deg%22%7D&iterationLimit=10000&motor=%7B%22quantity%22%3A1%2C%22name%22%3A%22NEO%22%7D&ratio=%7B%22magnitude%22%3A48%2C%22ratioType%22%3A%22Reduction%22%7D&startAngle=%7B%22s%22%3A30%2C%22u%22%3A%22deg%22%7D
-        self.arm_ff = ArmFeedforward(kS=0.42619, kG=0.4, kV=0.94, kA=0.01)
+        self.arm_ff = ArmFeedforward(kS=0.0, kG=0.4, kV=0.94, kA=0.01)
 
         spark_config.encoder.positionConversionFactor(math.tau * (1 / self.gear_ratio))
         spark_config.encoder.velocityConversionFactor(
