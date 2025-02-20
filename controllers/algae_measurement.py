1ea58bedb696431a876cd82be6ea8eb162324556
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2025-02-20 23:45:44 +1100

    less cooked multi-measure logic

--- controllers/algae_measurement.py
+++ controllers/algae_measurement.py
@@ -8,11 +8,11 @@ class AlgaeMeasurement(StateMachine):
     algae_manipulator_component: AlgaeManipulatorComponent
 
     def __init__(self) -> None:
-        self.stopped_counter = 0
         self.injector_1_starting_position = 0.0
         self.injector_2_starting_position = 0.0
         self.flywheel_top_starting_position = 0.0
         self.flywheel_bottom_starting_position = 0.0
+        self.injector_pos_delta_list = []
 
     def measure(self) -> None:
         self.engage()
@@ -29,7 +29,6 @@ class AlgaeMeasurement(StateMachine):
     @state(must_finish=True)
     def measuring(self, initial_call) -> None:
         if initial_call:
-            self.stopped_counter = 0
             self.injector_1_starting_position = (
                 self.algae_manipulator_component.get_injector_1_position()
             )
@@ -53,21 +52,36 @@ class AlgaeMeasurement(StateMachine):
             self.algae_manipulator_component.flywheel_bottom_position()
             - self.flywheel_bottom_starting_position
         ) >= 0.001:
-            injector_position_delta = (
-                self.algae_manipulator_component.get_injector_1_position()
-                - self.injector_1_starting_position
-            ) + (
-                self.algae_manipulator_component.get_injector_2_position()
-                - self.injector_2_starting_position
+            self.injector_pos_delta_list.append(
+                (
+                    self.algae_manipulator_component.get_injector_1_position()
+                    - self.injector_1_starting_position
+                )
+                + (
+                    self.algae_manipulator_component.get_injector_2_position()
+                    - self.injector_2_starting_position
+                )
             )
-            self.algae_manipulator_component.algae_size = scale_value(
-                injector_position_delta, 4.55, 1.62, 16.0, 17.0
-            )
-            self.stopped_counter = 0
-            self.done()
+
+            self.next_state("ball_retraction")
 
     @state(must_finish=True)
     def ball_retraction(self) -> None:
         self.algae_manipulator_component.intake()
         if self.algae_manipulator_component.has_algae():
+            self.next_state("multi_measure")
+
+    @state(must_finish=True)
+    def multi_measure(self) -> None:
+        if len(self.injector_pos_delta_list) < 4:
+            self.next_state("pre_measure")
+        else:
+            self.injector_pos_delta_list.pop(1)
+            self.algae_manipulator_component.algae_size = scale_value(
+                sum(self.injector_pos_delta_list) / len(self.injector_pos_delta_list),
+                4.55,
+                1.62,
+                16.0,
+                17.0,
+            )
             self.done()
