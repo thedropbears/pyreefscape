f3147d38b22bdad62f1932a4c628c4c5afad358a
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2025-02-20 21:31:05 +1100

    add logic to make the measure work with intaking

--- controllers/algae_measurement.py
+++ controllers/algae_measurement.py
@@ -1,8 +1,7 @@
-import math
-
-from magicbot import StateMachine, state
+from magicbot import StateMachine, state, timed_state
 
 from components.algae_manipulator import AlgaeManipulatorComponent
+from utilities.scalers import scale_value
 
 
 class AlgaeMeasurement(StateMachine):
@@ -10,36 +9,59 @@ class AlgaeMeasurement(StateMachine):
 
     def __init__(self) -> None:
         self.stopped_counter = 0
-        self.starting_position = 0.0
+        self.injector_1_starting_position = 0.0
+        self.injector_2_starting_position = 0.0
+        self.flywheel_top_starting_position = 0.0
+        self.flywheel_bottom_starting_position = 0.0
 
-    @state(
+    def measure(self) -> None:
+        self.engage()
+
+    @timed_state(
+        duration=0.5,
+        next_state="measuring",
         first=True,
         must_finish=True,
     )
+    def pre_measure(self) -> None:
+        pass
+
+    @state(must_finish=True)
     def measuring(self, initial_call) -> None:
         if initial_call:
             self.stopped_counter = 0
-            self.starting_position = (
-                self.algae_manipulator_component.get_injector_position()
+            self.injector_1_starting_position = (
+                self.algae_manipulator_component.get_injector_1_position()
+            )
+            self.injector_2_starting_position = (
+                self.algae_manipulator_component.get_injector_2_position()
+            )
+
+            self.flywheel_top_starting_position = (
+                self.algae_manipulator_component.flywheel_top_position()
+            )
+            self.flywheel_bottom_starting_position = (
+                self.algae_manipulator_component.flywheel_bottom_position()
             )
 
         self.algae_manipulator_component.measure_algae()
 
-        if math.isclose(
-            self.algae_manipulator_component.get_injector_velocity(), 0.0, abs_tol=0.1
-        ):
-            self.stopped_counter += 1
-
-        else:
-            self.stopped_counter = 0
-
         if (
-            self.algae_manipulator_component.bottom_flywheel_speed() >= 0.25
-            and self.algae_manipulator_component.top_flywheel_speed() >= 0.25
-        ):
-            self.algae_manipulator_component.algae_size = (
-                self.algae_manipulator_component.get_injector_position()
-                - self.starting_position
+            self.algae_manipulator_component.flywheel_top_position()
+            - self.flywheel_top_starting_position
+        ) >= 0.001 and (
+            self.algae_manipulator_component.flywheel_bottom_position()
+            - self.flywheel_bottom_starting_position
+        ) >= 0.001:
+            injector_position_delta = (
+                self.algae_manipulator_component.get_injector_1_position()
+                - self.injector_1_starting_position
+            ) + (
+                self.algae_manipulator_component.get_injector_2_position()
+                - self.injector_2_starting_position
+            )
+            self.algae_manipulator_component.algae_size = scale_value(
+                injector_position_delta, 4.55, 1.62, 16.0, 17.0
             )
             self.stopped_counter = 0
             self.done()
