8fe324550db31985e948e7f907dd544eb1e3a79d
callum <callum.wong@thedropbears.org.au>
2025-02-21 19:35:40 +1100

    rename CoralAuto to CentreAuto, set correct trajectories

--- autonomous/coral_auto.py
+++
@@ -1,8 +0,0 @@
-from autonomous.auto_base import AutoBase
-
-
-class CoralAuto(AutoBase):
-    MODE_NAME = "Coral Shooter"
-
-    def __init__(self):
-        super().__init__(["StartToBranchH", "BranchHToAlgaeGH", "AlgaeGHToShoot"])
