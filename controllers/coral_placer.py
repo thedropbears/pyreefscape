3716f7eb7d9ffd37e1b0e9ce0b355f6d3abab6cc
LH <liam.howe2@education.nsw.gov.au>
2025-02-21 16:43:15 +1100

    Created coral placer state machine

--- controllers/coral_placer.py
+++ controllers/coral_placer.py
@@ -1,17 +1,34 @@
-from magicbot import StateMachine, timed_state
+import math
+
+from magicbot import StateMachine, state, timed_state, tunable
 
 from components.coral_placer import CoralPlacerComponent
+from components.wrist import WristComponent
 
 
 class CoralPlacer(StateMachine):
     coral_placer_component: CoralPlacerComponent
+    wrist: WristComponent
+
+    CORAL_PLACE_ANGLE = tunable(math.radians(-5.0))
 
     def __init__(self):
         pass
 
-    def place(self):
+    def place(self) -> None:
         self.engage()
 
-    @timed_state(duration=2.0, first=True, must_finish=True)
-    def placing(self):
+    @state(first=True, must_finish=True)
+    def raising(self, initial_call: bool) -> None:
+        if initial_call:
+            self.wrist.tilt_to(self.CORAL_PLACE_ANGLE)
+        if self.wrist.at_setpoint():
+            self.next_state("placing")
+
+    @timed_state(duration=1, must_finish=True)
+    def placing(self) -> None:
         self.coral_placer_component.place()
+
+    def done(self) -> None:
+        self.wrist.go_to_neutral()
+        super().done()
