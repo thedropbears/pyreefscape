343c3d973f5ca89cbcb83797ca3e62576304ea83
Lucien Morey <lucien@thedropbears.org.au>
2025-02-22 20:53:48 +1100

    fix path name

--- autonomous/multi_algae_auto.py
+++ autonomous/multi_algae_auto.py
@@ -2,7 +2,7 @@ from autonomous.auto_base import AutoBase
 
 
 class CentreAuto(AutoBase):
-    MODE_NAME = "Centre F>GH>IJ>EF"
+    MODE_NAME = "Centre G>GH>IJ>EF"
 
     def __init__(self):
         super().__init__(
