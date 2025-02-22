cb74cede5f51fae8ff7c81bce53802a50cc6cf51
LH <liam.howe2@education.nsw.gov.au>
2025-02-22 11:11:20 +1100

    Actualy removed the import in ballistics which was causing the error -_-

--- components/ballistics.py
+++ components/ballistics.py
@@ -8,7 +8,6 @@ from magicbot import feedback
 from wpilib import DriverStation
 from wpimath.geometry import Pose2d, Rotation2d, Translation2d
 
-from autonomous.auto_base import AutoBase
 from components.chassis import ChassisComponent
 from components.injector import InjectorComponent
 from components.led_component import LightStrip
@@ -33,8 +32,6 @@ class BallisticsSolution:
 
 
 class BallisticsComponent:
-    auto_base: AutoBase
-
     chassis: ChassisComponent
     status_lights: LightStrip
     shooter_component: ShooterComponent
