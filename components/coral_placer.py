209e158ab3829e5375b6b674715cb1ba74be9e43
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2025-02-23 15:05:33 +1100

    make stuff work a bit better

--- components/coral_placer.py
+++ components/coral_placer.py
@@ -8,14 +8,16 @@ class CoralPlacerComponent:
     servo_resting_angle = tunable(70)
     servo_active_angle = tunable(180)
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.servo = Servo(PwmChannel.CORAL_SERVO)
+
+    def setup(self) -> None:
         self.desired_angle = self.servo_resting_angle
 
-    def coral_latch_open(self):
+    def coral_latch_open(self) -> None:
         self.servo.setAngle(self.servo_active_angle)
 
-    def coral_latch_closed(self):
+    def coral_latch_closed(self) -> None:
         self.servo.setAngle(self.servo_resting_angle)
 
     def execute(self):
