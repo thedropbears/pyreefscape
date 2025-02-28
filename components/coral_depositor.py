37a75115b0601981f94f5f23724b68333a1f68a3
callum <callum.wong@thedropbears.org.au>
2025-02-28 17:13:06 +1100

    scrap the placer, here comes the depositor!!!

---
+++ components/coral_depositor.py
@@ -0,0 +1,21 @@
+from wpilib import Servo
+
+from ids import PwmChannel
+
+
+class CoralDepositorComponent:
+    holding_angle = 0
+    depositing_angle = 90
+
+    def __init__(self) -> None:
+        self.servo = Servo(PwmChannel.CORAL_SERVO)
+        self.retract()
+
+    def deposit(self):
+        self.desired_angle = self.depositing_angle
+
+    def retract(self):
+        self.desired_angle = self.holding_angle
+
+    def execute(self):
+        self.servo.set(self.desired_angle / 180.0)
