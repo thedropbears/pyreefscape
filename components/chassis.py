47a61da0eaf272bd8db34bc8b99477c63a0cbe39
Lucien Morey <lucien@thedropbears.org.au>
2025-03-05 20:47:38 +1100

    retune gains

--- components/chassis.py
+++ components/chassis.py
@@ -250,7 +250,7 @@ class ChassisComponent:
         self, track_width: float, wheel_base: float, swerve_config: SwerveConfig
     ) -> None:
         self.imu = Pigeon2(0)
-        self.heading_controller = PIDController(4.5, 0.0, 0.01)
+        self.heading_controller = PIDController(4.5, 0.0, 0.08)
         wpilib.SmartDashboard.putData(
             "Chassis heading_controller", self.heading_controller
         )
