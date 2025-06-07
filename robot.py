5a264bef13787004829e637dacd2a759962eaf15
LeanMeanBeanMachine4774 <asher.broadhead@thedropbears.org.au>
2025-06-07 12:39:26 +1000

    remove preclimb checks from robotpy

--- robot.py
+++ robot.py
@@ -245,14 +245,8 @@ class MyRobot(magicbot.MagicRobot):
 
         if self.gamepad.getYButton():
             self.climber_state_machine.deploy()
-        if self.gamepad.getAButton():
-            if (
-                self.climber_state_machine.current_state == "pre_climbing"
-                and self.climber_state_machine.is_ready_to_climb()
-            ) or self.climber_state_machine.current_state == "retracting":
-                self.climber_state_machine.retract()
-            else:
-                self.climber_state_machine.pre_climb()
+        if self.gamepad.getAButton() and self.climber_state_machine.is_ready_to_climb():
+            self.climber_state_machine.retract()
 
         if self.gamepad.getBButton():
             self.reef_intake.done()
